2e6d65a0adf8e7e85dc85f3583e123a7530ea48d
glvastest <glebvas1@i.com>
2025-01-18 04:48:40 +0300

    brush randomization

--- game_brush_manager.py
+++ game_brush_manager.py
@@ -1,4 +1,5 @@
 from math import sqrt
+import random
 from PyQt5 import QtWidgets, QtCore
 from functools import partial
 
@@ -14,14 +15,23 @@ class BrushManager(object):
         self.fillAllCells.clicked.connect(self.changeAllButtonsStateInGame)
 
     def paintInPlace(self, x : int, y : int):
+        size = self.brushSize
+        if self.brushRandomSize.isChecked():
+            size = random.randint(0, 5)
+
         for i in range(1 - self.brushSize, self.brushSize):
             for j in range(1 - self.brushSize, self.brushSize):
-                if self.circleBrush:
-                    if sqrt(i * i + j * j) > self.brushSize:
+            
+                if self.brushSetRound.isChecked():
+                    if i ** 2 + j ** 2 >= size ** 2:
                         continue
                 x1 = (x + i + self.xFieldSize) % self.xFieldSize
                 y1 = (y + j + self.yFieldSize) % self.yFieldSize
-                self.changeButtonStateInGame(x1, y1)
+
+                if self.brushRandomState.isChecked():
+                    self.changeButtonState(x1, y1, random.randint(0, self.calc.thisRule.generationsCount))
+                else:
+                    self.changeButtonStateInGame(x1, y1)
 
     def changeBrushSize(self):
         self.brushSize = self.brushSizeSpinBox.value()
